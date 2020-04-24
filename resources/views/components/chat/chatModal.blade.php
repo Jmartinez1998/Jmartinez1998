45fa29d256a2434de01841ccdff9b4bfea642544
karla burciaga delgado <karla_burciaga_d@outlook.com>
2020-04-24 15:52:54 -0500

    vistas del chat

--- resources/views/components/chat/chatModal.blade.php
+++ resources/views/components/chat/chatModal.blade.php
@@ -1,49 +1,60 @@
- <!-- MODAL MESSENGER -->
- <section class="section">
-    <div class="container">
-      <div class="row"> 
-            <div class="">
-              <!-- Modal: modalPoll -->
-              <div class="modal fade left" id="modalPoll" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true" data-backdrop="false">
-                <div class="modal-dialog modal-full-height modal-left modal-notify  modal-info" role="document" >
-                  <div class="modal-content">
-                    <!--Header-->
-                    <div class="modal-header" style="background-color:#b8c829;"
-                      <p class="heading lead text-center text-white"><strong class="text-white">¿Tienes alguna duda? Contactanos por aquí</strong>
-                      </p>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" class="white-text">×</span>
-                      </button>
-                    </div>
-                    <!--Body-->
-                    <div class="modal-body" style="background-color:#c9e3d936">
-                      <div class="form-group">
-                        <label for="recipient-name" class="col-form-label">Últimos mensajes:</label>
-                        <div class="jumbotron">
-                          <h3>Hello, world!</h3>
-                        </div>
-                      </div>
-                        <form>
-                          <div class="form-group">
-                            <label for="message-text" class="col-form-label">Mensaje:</label>
-                            <textarea class="form-control" id="message-text"></textarea>
-                          </div>
-                        </form>
-                    </div>
-                    <!--Footer-->
-                    <div class="modal-footer justify-content-center" style="height:10%;background-color:#c9e3d936">
-                      <a  type="button" class="btn btn-primary waves-effect waves-light" >Enviar Mensaje
-                        <i class="fa fa-paper-plane ml-1"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- Modal: modalPoll -->
-            </div>
-      </div>
-    </div>
-  </section>
+<!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Audi - Laguna</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- Google Font -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="/assets/images/logo-banner.png">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Plugins CSS -->
+    <link href="libs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    
+    <!--<link href="assets/css/component/colors/blue.css" rel="stylesheet" type="text/css">-->
+    <link href="assets/css/green.css" rel="stylesheet" type="text/css">
+     <script src="https://js.pusher.com/5.1/pusher.min.js"></script>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+     @livewireStyles
+     @livewireScripts
+</head>
+<body id="page-top" data-offset="51">
+   
 
-  <!-- CIERRE DEL MODAL -->
+    <!-- Begin Intro Section -->
+    <section id="intro" style="margin-top:-1.8rem; padding-top:4rem;">
+        <div class="intro-body text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 intro-content">
+                        <!-- //.page-scroll -->   @livewire("chat")
+                    </div>
+                    <!-- //.intro-content -->
+                </div>
+                <!-- //.row -->
+            </div>
+            <!-- //.container -->
+        </div>
+        <!-- //.intro-body -->
+    </section>
+   
+    
+    <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+    <!-- Animation JS (Optional) -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+
+
+
