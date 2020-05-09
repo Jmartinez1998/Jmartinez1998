2d57d0ba7e9075617b10f1465289c057a32db2b5
karla burciaga delgado <karla_burciaga_d@outlook.com>
2020-05-09 14:41:37 -0500

    diseño cotizacion

---
+++ resources/views/emails/respuestas/cotizacionAdmin.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+</head>
+  <body>
+  <div class="container">
+      <img src="" style="width:30%;margin-left: 35%;margin-right: 35%;">
+      <h1 style="text-align:center">¡Usted a respondido a la siguiente cotización!</h1>
+      <h2>Asunto: {{$request->Asunto}} , FECHA: {{$request->fecha}}</h2>
+    <hr>
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+    <div class="jumbotron" style="border-radius:10px">
+      <div class="container">
+        {!! $request->mensaje!!}
+      </div> 
+    </div>
+    </div>
+    <div class="col-md-2"></div>
+    <!-- Latest compiled and minified JavaScript -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+  </body>
+</html> 
